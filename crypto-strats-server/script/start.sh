77e98612c86ce3064dde450dd6038b9440e0be7e
root <root@ip-172-31-60-255.ec2.internal>
2015-05-29 00:25:52 +0000

    fixing maven opts

--- crypto-strats-server/script/start.sh
+++ crypto-strats-server/script/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 cd ..
-export MAVEN_OPTS="-Djavax.net.ssl.trustStore=./certs/jssecacerts"
+export MAVEN_OPTS="-Djavax.net.ssl.trustStore=./certs/jssecacerts -Dlog4j.configuration=file:./src/main/resources/log4j2.xml -Xmx8192m -XX:MaxPermSize=1024m"
 mvn install vertx:runMod
